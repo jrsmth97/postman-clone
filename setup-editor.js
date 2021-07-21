7ccbf2f87b79fcf9c10a88dde171737ed0a5cd5b
jrsmth97 <69376504+jrsmth97@users.noreply.github.com>
2021-07-21 22:26:28 +0700

    Update setup-editor.js

--- setup-editor.js
+++ setup-editor.js
@@ -1,7 +1,7 @@
-import { EditorState, basicSetup } from "@codemirror/basic-setup";
-import { defaultTabBinding } from "@codemirror/commands";
-import { EditorView, keymap } from "@codemirror/view";
-import { json } from "@codemirror/lang-json";
+import { EditorState, basicSetup } from "./node_modules/@codemirror/basic-setup";
+import { defaultTabBinding } from "./node_modules/@codemirror/commands";
+import { EditorView, keymap } from "./node_modules/@codemirror/view";
+import { json } from "./node_modules/@codemirror/lang-json";
 
 export default function setupEditors() {
   const jsonRequestBody = document.querySelector("[data-json-request-body]");
